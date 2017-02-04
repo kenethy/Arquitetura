7d422324755a3a566d58c148cb4e259a05f66ed8
m2m <m2m@W10PROCON03>
2017-02-03 23:10:46 -0300

    REMOCAO DO TESTE

--- src/Teste.java
+++
@@ -1,8 +0,0 @@
-
-public class Teste {
-	public static void main(String[] args){
-		int jump = Integer.parseUnsignedInt("11111111111111111111111111111111", 2) & 0xFFFFFFFF;
-		Integer.parseInt("1", 2);
-		System.out.println(jump);
-	}
-}
